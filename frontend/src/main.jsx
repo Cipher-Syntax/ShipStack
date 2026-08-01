90720db5396dd1dc5ec69a0734f342c77922608e
Justine Toong <programmerjustine@gmail.com>
2026-08-01 09:02:45 +0800

    Added: tailwindcss/vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './apps/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './styles/index.css';
+import App from './apps/App';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
